1e598e769c64de6ed3c78f7eed94734096cb7601
alankaiko <alamdfs@hotmail.com>
2020-03-19 20:40:21 -0300

    primeirio commit

---
+++ src/app/views/operacoes/operacoes.module.ts
@@ -0,0 +1,40 @@
+import { CapturaComponent } from './captura.component';
+import { LaudoComponent } from './laudo.component';
+import { ListaatendimentoComponent } from './listaatendimento.component';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { ProgressbarModule } from 'ngx-bootstrap/progressbar';
+import { PopoverModule } from 'ngx-bootstrap/popover';
+import { PaginationModule } from 'ngx-bootstrap/pagination';
+import { CollapseModule } from 'ngx-bootstrap/collapse';
+import { CarouselModule } from 'ngx-bootstrap/carousel';
+import { TabsModule } from 'ngx-bootstrap/tabs';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { BaseRoutingModule } from './../base/base-routing.module';
+import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+
+@NgModule({
+
+  imports: [
+    CommonModule,
+    FormsModule,
+    BaseRoutingModule,
+    BsDropdownModule.forRoot(),
+    TabsModule,
+    CarouselModule.forRoot(),
+    CollapseModule.forRoot(),
+    PaginationModule.forRoot(),
+    PopoverModule.forRoot(),
+    ProgressbarModule.forRoot(),
+    TooltipModule.forRoot()
+  ],
+  declarations: [
+    ListaatendimentoComponent,
+    LaudoComponent,
+    CapturaComponent
+  ]
+})
+export class OperacoesModule { }
